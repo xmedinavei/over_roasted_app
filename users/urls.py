bef72a7cf04efacd61c4089db8301556a20c9641
Xavier Medina <xmedinavei@gmail.com>
2021-02-16 10:47:17 -0500

    Users modelu is working totally

---
+++ users/urls.py
@@ -0,0 +1,24 @@
+# Django
+from django.urls import path
+
+# View
+from users import views
+ 
+ 
+urlpatterns = [
+    path(
+        route='login/',
+        view=views.LoginView.as_view(),
+        name='login'
+    ),
+    path(
+        route='logout/',
+        view=views.LogoutView.as_view(),
+        name='logout'
+    ),
+    path(
+        route='signup/',
+        view=views.SignupView.as_view(),
+        name='signup'
+    ),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
+# Django
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.views.generic import FormView
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth import logout
 
-# Create your views here.
+#Models
+from django.contrib.auth.models import User
+
+#Forms
+from users.forms import SignupForm 
+
+
+class SignupView(FormView):
+    """Users sign up view."""
+
+    template_name = 'users/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('users:login')
+
+    #ver doc de FormView.
+    #Sobreescribimos el form_valid para que guarde los datos
+    def form_valid(self, form):
+        """Save form data."""
+        form.save()
+        return super().form_valid(form)
+
+class LoginView(auth_views.LoginView):
+    """Login view."""
+    template_name = 'users/login.html'
+
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """Logout view."""
+    template_name = 'users/logged_out.html'
+    
